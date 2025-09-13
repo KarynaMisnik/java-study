a13775583ab066581d35b3550c972367d0157b02
KarynaMisnik <karinamisnik94@gmail.com>
2025-09-13 12:15:00 +0300

    ex 1.2

---
+++ Grade.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Grade{
+public static void main(String[] args){
+
+Scanner scanner  = new Scanner(System.in);
+System.out.println("Type your grade: ");
+
+int grade = scanner.nextInt();
+
+}
+}
